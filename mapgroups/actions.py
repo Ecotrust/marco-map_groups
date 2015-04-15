06fe776c5d52fc9d74b1c6e37f08674d2c7befe1
Seth Hill <seth@pointnineseven.com>
2015-04-15 10:42:53 -0700

    MP-736: "leave group" now unshares all features as well.  This introduces dependencies on scenarios, visualize, drawing, and features (indirectly)

--- mapgroups/actions.py
+++ mapgroups/actions.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group
+import itertools
 from mapgroups.models import MapGroup, MapGroupMember, Invitation, ActivityLog
 
 
@@ -44,6 +45,25 @@ def leave_non_owned_map_group(user, group):
 
     group.mapgroupmember_set.get(user=user).delete()
     group.permission_group.user_set.remove(user)
+
+    pgroup = group.permission_group
+
+    # Unshare any features that were shared with the old group
+
+    # TODO: Figure out a way to find all m2m models that are related to
+    # Group, iterate over each, figure out if each item descends from 'Feature',
+    # and then unshare each of those.
+    # Right now, if we were to add a new kind of related feature that could
+    # be shared, we'd have to remember to include it here by hand.
+    items = itertools.chain(
+        pgroup.scenarios_scenario_related.filter(user=user),
+        pgroup.visualize_bookmark_related.filter(user=user),
+        pgroup.scenarios_leaseblockselection_related.filter(user=user),
+        pgroup.drawing_aoi_related.filter(user=user),
+        pgroup.drawing_windenergysite_related.filter(user=user)
+    )
+    for item in items:
+        item.unshare_with(pgroup)
 
     return True
 
