da99db279054843874fef636cc0370101254c7c9
Seth Hill <seth@pointnineseven.com>
2015-04-30 10:11:59 -0700

    POR-379: Add in the creation date field

--- mapgroups/models.py
+++ mapgroups/models.py
@@ -73,6 +73,7 @@ class MapGroup(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     owner = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_created=True)
 #     icon = models.URLField()
     image = models.ImageField(upload_to=map_group_image_path, #'group_images/%Y%m%d/',
                               width_field='image_width',
