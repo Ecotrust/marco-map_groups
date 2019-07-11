35de687173028b8c8b9c75512059cb7357d357c3
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-11 13:50:01 -0700

    fixing migration

--- mapgroups/migrations/0001_initial.py
+++ mapgroups/migrations/0001_initial.py
@@ -66,7 +66,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=255)),
                 ('slug', models.SlugField(max_length=255)),
-                ('blur', models.CharField(max_length=512)),
+                ('blurb', models.CharField(max_length=512)),
                 ('is_open', models.BooleanField(default=False, help_text='If false, users must be invited or request to join this group')),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=django.db.models.deletion.CASCADE)),
                 ('permission_group', models.ForeignKey(to='auth.Group', unique=True, on_delete=django.db.models.deletion.CASCADE)),
